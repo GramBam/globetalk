bc9c5459e3d6cc9eae7294b64a2c2d6c12cd3da2
Graham <graham.moss@hotmail.com>
2023-03-30 19:16:51 -0400

    server/db setup

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
-function App() {
-  return <div className="App">Working!</div>;
-}
-
-export default App;
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,37 @@
+import * as dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/database";
+import path from "path";
 
+import http from "http";
+
+dotenv.config();
 const app = express();
-const PORT: number = 5000;
+new http.Server(app);
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("Working!");
 });
 
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Something is wrong :)" });
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(
-    "The application is listening " + "on port http://localhost:" + PORT
-  );
+  console.log(`Server started on PORT ${PORT}`);
 });
